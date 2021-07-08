7957af937fd4a6992b53634e2e1fd0164f127fd4
Leart1234 <61915340+Leart1234@users.noreply.github.com>
2021-07-09 01:21:20 +0200

    initial commit

---
+++ RealEstate/resources/views/hero.blade.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real Estate</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="{{ URL::asset('css/hero.css') }}">
+</head>
+<body>
+<div class="hero-image">
+  <div class="hero-text">
+    <h1 class="head">Beautiful<br>homes made<br>for you</h1>
+    <p>In oculis quidem se esse admonere interesse enim maxime placeat, facere possimus,
+     omnis. Et quidem faciunt, ut labore et accurate disserendum et harum quidem exercitus.</p>
+     <span class="hero-p">See all listings
+       <img src="/images/arrow.png" style="padding-left:12px; cursor:pointer;" alt="">
+     </span>
+  </div>
+</div>
+</body>
+</html>
