ee96c1cd13007e67e916e2252720801e19c827ae
BardhKorca <bardh.korqa@gmail.com>
2021-07-08 17:49:28 +0200

    content

---
+++ RealEstate/resources/views/content.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Content</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width , initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+    <link rel="stylesheet" type="text/css" href="../css/content.css">
+</head>
+<body>
+<div class="section">
+    <div class="container">
+        <div class="content-section">
+            <div class="title">
+                <h1>You're in good hands</h1>
+            </div>
+            <div class="content">
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing e</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Odit praesentium est perspiciatis suscipit, saepe corporis 
+                    perferendis autem fuga, consequatur mollitia nihil. Debitis, natus. 
+                    Quisquam vero ratione quos sint in assumenda.
+                </p>
+                <div class="button">
+                    <a href="">Learn More -></a>
+                </div>
+            </div>   
+        </div>
+            <div class="image-section">
+                <img src="../images/contentimg.jpg" alt="">
+            </div>
+    </div>
+</div>
+<div class="content-section2">
+    <div class="inner-width">
+        <h1>You're in good hands</h1>
+        <div class="border"></div>
+            <div class="content-section2-row">
+            <div class="content-section2-col">
+                <div class="content2">
+                    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Porro, molestiae dolorum. Possimus consequuntur sapiente aspernatur, 
+                        eius asperiores ex vero obcaecati ducimus laudantium 
+                        voluptatum ut nisi corporis quos pariatur repudiandae cupiditate!</p>
+                    
+                    <a href="#">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>  
+</div>
+
+</body>
+</html>
+
+
