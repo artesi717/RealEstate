4c4601f015dd28529917c6391723b1cecbfcfe48
GezimSejdiuGsD <76073533+GezimSejdiuGsD@users.noreply.github.com>
2021-07-08 15:35:50 +0200

    Navbar created

---
+++ RealEstate/resources/views/navbar.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="../css/navbar1.css">
+
+<title>Page Title</title>
+</head>
+    <body>
+
+    <style>
+              .myButton {
+	        background-color:#ffac12;
+	        cursor:pointer;
+	        color:black;
+            font-family: 'Manrope', sans-serif;
+	        font-size:16px;
+	        padding:10px 31px;
+            margin-right:-30%;
+            border-radius:0px 16px 0px   0px;
+        }
+
+        body   {
+            background-color:black;
+            background-image: url('https://www.nationalobserver.com/sites/nationalobserver.com/files/img/2021/04/20/pexels-binyamin-mellish-186077.jpg');
+            background-size:100%;
+            font-family: 'Manrope', sans-serif;
+            padding-top:15px;
+
+        }
+
+        .nav-link {
+            color:white;
+            font-size:14px;
+            padding-left:30px;
+            padding-right:30px;
+        }
+        .nav-item {
+            padding-left:45px;
+            padding-right:45px;
+            padding-top:6px;
+            font-family: 'Inter', sans-serif;
+            margin-left:-5%;
+        }
+        .nav-link {
+            text-align:center;
+            float:center;
+
+        }
+
+        .li {
+      text-align: center;
+      margin-right:60px;
+        }
+
+        i {
+            color:white;
+            padding-left: 10px;
+            
+        }
+        .logo1{
+            width:100%;
+        }
+    </style>
+
+<div class="navtext">
+	<nav class="navbar navbar-expand-lg navbar-transparent bg-transparent static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+          <img class="logo1" src="https://www.hommeet.com/wp-content/uploads/2020/06/logo-05.png" alt="" style="width:35%;">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+<div>    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nav link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nav link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nav link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nav link</a>
+        </li>
+
+      </ul>
+  <a href="" class="myButton">Work with us   <i class="fas fa-long-arrow-alt-right"></i></a></div></div>
+
+
+    </div>
+  </div></div>
+
+  </>
+</nav>
+</div>
+
+
+
+    </body>
+</html>
