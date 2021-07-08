a7a891e1f0b346f99be171adf64653b2d89b248b
artesi717 <artesberisha0981@gmail.com>
2021-07-08 16:23:29 +0200

    first commit

---
+++ RealEstate/resources/views/homepage2.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap" rel="stylesheet">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<link rel="stylesheet" type="text/css" href="../css/homepage2.css">
+
+
+
+</head>
+<body>
+   
+    <img class="foto1" src="http://holtzmanpartners.com/wp-content/uploads/2021/06/Advisory.jpg" alt="">
+    
+    <div class="divi11">
+    <hr class="colored" />
+    <h1 class="emri1">You're in good hands</h1>
+    <p class="text2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries,</p>
+    <br><br>
+    <a href="#" class="myButton">Learn more    <i class="fas fa-long-arrow-alt-right"></i></a>
+</div>
+</body>
+</html>
