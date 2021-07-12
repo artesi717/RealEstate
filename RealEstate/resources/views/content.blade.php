a9b6370f67048726edba90cbfd2a2988cab0f95f
BardhKorca <bardh.korqa@gmail.com>
2021-07-12 17:03:37 +0200

    content

--- RealEstate/resources/views/content.blade.php
+++ RealEstate/resources/views/content.blade.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
+<html lang="en">
 <html>
 <head>
 	<title>Content</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width , initial-scale=1.0">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
-    <link rel="stylesheet" type="text/css" href="../css/content.css">
+    <link rel="stylesheet" href="{{ URL::asset('css/content.css') }}">
 </head>
 <body>
 <div class="section">
@@ -27,27 +27,10 @@
             </div>   
         </div>
             <div class="image-section">
-                <img src="../images/contentimg.jpg" alt="">
+                <img src="/images/contentimg.jpg" alt="">
             </div>
     </div>
-</div>
-<div class="content-section2">
-    <div class="inner-width">
-        <h1>You're in good hands</h1>
-        <div class="border"></div>
-            <div class="content-section2-row">
-            <div class="content-section2-col">
-                <div class="content2">
-                    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                        Porro, molestiae dolorum. Possimus consequuntur sapiente aspernatur, 
-                        eius asperiores ex vero obcaecati ducimus laudantium 
-                        voluptatum ut nisi corporis quos pariatur repudiandae cupiditate!</p>
-                    
-                    <a href="#">Learn More</a>
-                </div>
-            </div>
-        </div>
-    </div>  
+</div>  
 </div>
 
 </body>
