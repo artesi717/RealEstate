d2ddbabef149dc6fd4e5b7250b518d3aed4628f8
Leart1234 <61915340+Leart1234@users.noreply.github.com>
2021-07-13 09:39:42 +0200

    initial commit

--- RealEstate/resources/views/hero.blade.php
+++ RealEstate/resources/views/hero.blade.php
@@ -14,66 +14,17 @@
 
 </head>
 <body>
-  <style>
-.myButton {
-	        background-color:#ffac12;
-	        cursor:pointer;
-	        color:black;
-            font-family: 'Manrope', sans-serif;
-	        font-size:16px;
-	        padding:10px 31px;
-            margin-right:-30%;
-            border-radius:0px 16px 0px   0px;
-        }
-        body   {
-            background-color:black;
-            background-image: url('https://www.nationalobserver.com/sites/nationalobserver.com/files/img/2021/04/20/pexels-binyamin-mellish-186077.jpg');
-            background-size:100%;
-            font-family: 'Manrope', sans-serif;
-            padding-top:15px;
-        }
-        .nav-link {
-            color:white;
-            font-size:14px;
-            padding-left:30px;
-            padding-right:30px;
-        }
-        .nav-item {
-            padding-left:45px;
-            padding-right:45px;
-            padding-top:6px;
-            font-family: 'Inter', sans-serif;
-            margin-left:-5%;
-        }
-        .nav-link {
-            text-align:center;
-            float:center;
-        }
-        .li {
-      text-align: center;
-      margin-right:60px;
-        }
-        i {
-            color:white;
-            padding-left: 10px;
-            
-        }
-        .logo1{
-            width:100%;
-        }
-    </style>
-
 <div class="navtext">
 	<nav class="navbar navbar-expand-lg navbar-transparent bg-transparent static-top">
   <div class="container">
     <a class="navbar-brand" href="#">
           <img class="logo1" src="https://www.hommeet.com/wp-content/uploads/2020/06/logo-05.png" alt="" style="width:35%;">
         </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-<div>    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
+     <div>    
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+  <ul class="navbar-nav ml-auto" style="
+    margin: 31px;
+    margin-left: 23px;">
         <li class="nav-item">
           <a class="nav-link" href="#">Nav link</a>
         </li>
@@ -86,26 +37,28 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Nav link</a>
         </li>
-
+        
       </ul>
-  <a href="" class="myButton">Work with us   <i class="fas fa-long-arrow-alt-right"></i></a></div></div>
-
-
+      <ul class="nav navbar-nav navbar-right"style="  margin: 35px -59px 39px -3px;
+    left: 74px;">
+        <a href="" class="mybtn">Work with us<i class="fas fa-long-arrow-alt-right"></i></a></a></li>
+      </ul>
+    
     </div>
-  </div></div>
+  </div>
+</div>
 </nav>
 </div>
-    
-      <header>
+<header>
   <div class="hero-text">
     <h1 class="head">Beautiful<br>homes made<br>for you</h1>
     <p>In oculis quidem se esse admonere interesse enim maxime placeat, facere possimus,
      omnis. Et quidem faciunt, ut labore et accurate disserendum et harum quidem exercitus.</p>
      <span class="hero-p">See all listings
-       
+       <img src="/images/arrow.png" style="padding-left:12px; cursor:pointer;" alt="">
      </span>
   </div>
 </div>
-      </header>
+</header>
 </body>
 </html>
